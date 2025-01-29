a032936b4b4cd6df37259df7bdea4cc63b093ef8
aletris <104312459+cyrus0729@users.noreply.github.com>
2025-01-29 19:00:50 +0800

    Revert "d"
    This reverts commit 65758cbd86dc90f0751dbfa57286ce15da9b2fab.

--- target/debug/build/gmp-mpfr-sys-4205103809d042b5/build_script_build-4205103809d042b5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cyrus\Documents\GitHub\sutki\target\debug\build\gmp-mpfr-sys-4205103809d042b5\build_script_build-4205103809d042b5.exe: C:\Users\cyrus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gmp-mpfr-sys-1.6.4\build.rs
-
-C:\Users\cyrus\Documents\GitHub\sutki\target\debug\build\gmp-mpfr-sys-4205103809d042b5\build_script_build-4205103809d042b5.d: C:\Users\cyrus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gmp-mpfr-sys-1.6.4\build.rs
-
-C:\Users\cyrus\.cargo\registry\src\index.crates.io-6f17d22bba15001f\gmp-mpfr-sys-1.6.4\build.rs:
